9ee9818130ce33e8452d92140f13a5b996bb960e
efra8ab <efra8ab@gmail.com>
2025-10-29 01:38:30 -0600

    cleaned server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Helloooo dog');
-});
+app.use('/', require('./Routes/lesson1Route'));
 
 app.listen(port);
 console.log('Web server is listening at port ' + port);
